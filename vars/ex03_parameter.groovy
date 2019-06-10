a7caef7228f52eb46701cb5a7f5f962321e11b81
Jason Yang <jason_yang490@yahoo.com>
2019-06-09 20:56:50 -0700

    check in

---
+++ vars/ex03_parameter.groovy
@@ -0,0 +1,17 @@
+def call() {
+
+  def opts = [
+    name: 'what is your name'
+  ]
+
+  pipeline {
+    agent any
+    stages { 
+      stage('Example') {
+        steps {
+          echo "Hi, ${name}" 
+        }
+      }
+    }
+  }
+}
